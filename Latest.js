a0ec1500d139436d9afd106d8e72cfe3b05bf236
Manasa <165789253+manasadharmarapu@users.noreply.github.com>
2024-04-02 17:58:50 +0530

    Add files via upload

---
+++ Latest.js
@@ -0,0 +1,12 @@
+function Latest() {
+
+    return(
+
+        <div className="container">
+                <div className="latest">
+                    <h1>Latest Books</h1>
+                </div>
+        </div>
+    )
+}
+export default Latest;
