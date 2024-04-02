559945e98b35da44597789157097d53dacfbed8e
Manasa <165789253+manasadharmarapu@users.noreply.github.com>
2024-04-02 17:57:30 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,23 @@
+function Home() {
+
+    return(
+ 
+        <div className="header">
+        <div className="container">
+            <div className="home">
+           <div className="logo">
+            <h1>Book Library</h1>
+           </div>
+
+           <div className="navbar">
+             <ul>
+                <li><a href="add">Add Book</a></li>
+                <li><a href="view">View Book</a></li>
+             </ul>
+           </div>
+           </div>
+        </div>
+        </div>
+    )
+}
+export default Home;
