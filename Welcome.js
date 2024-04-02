ffdd9f04c0d8a46b49f6769d0b9356360af26567
Manasa <165789253+manasadharmarapu@users.noreply.github.com>
2024-04-02 17:59:53 +0530

    Add files via upload

---
+++ Welcome.js
@@ -0,0 +1,14 @@
+function Welcome() {
+
+    return(
+
+        <div className="welcome">
+        <div className="container">
+            <div className="heading">
+                <h1>Welcome to Book Library</h1>
+            </div>
+        </div>
+        </div>
+    )
+}
+export default Welcome;
