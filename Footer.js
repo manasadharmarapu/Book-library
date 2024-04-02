18368739200b6c9df33dc968e59084301941389a
Manasa <165789253+manasadharmarapu@users.noreply.github.com>
2024-04-02 17:57:08 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,14 @@
+function Footer() {
+
+    return(
+
+        <div className="footer">
+        <div className="container">
+            <div className="para">
+           <p>copyright 2023. All Rights Served</p>
+           </div>
+        </div>
+        </div>
+    )
+}
+export default Footer;
